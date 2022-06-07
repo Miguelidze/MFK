8e5f93de045d86cf6a01edd1aaf900be74173220
Miguelidze <migueldeorpesa@gmail.com>
2022-06-07 18:44:52 +0200

    fix fonts etc

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -4,7 +4,7 @@ function myFunction() {
 }
 // Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn,.dropbtn-text,.dropdown__picture')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
