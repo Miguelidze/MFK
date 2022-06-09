97abd56fac0c43d21702920031a5996a13ba8314
Miguelidze <migueldeorpesa@gmail.com>
2022-06-10 01:19:15 +0200

    add js for pages

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,7 @@
-// operations.html for
+// operations.html
+// ved.html 
+// specialchecks.html
+// distance.htmlfor
 $("#tarifs").click(function (e) {
     e.preventDefault();
     $("#tarifs").addClass("current-link");
@@ -33,24 +36,50 @@ $('body').on('click','#showmore.less',function(e){
 
 // accountopen.html for
 
-$("#resident,#noresident,#up,#custom").click(function (e) {
+$("#resident").click(function (e) {
     e.preventDefault();
-    $("#resident").toggleClass("current-link");
-    $("#noresident").toggleClass("current-link");
-    $("#up").toggleClass("current-link");
-    $("#custom").toggleClass("current-link");
-    $("#resident-item").toggleClass("active");
-    $("#noresident-item").toggleClass("active");
-    $("#up-item").toggleClass("active");
-    $("#ncustom-item").toggleClass("active");
+    $("#resident").addClass("current-link");
+    $("#noresident").removeClass("current-link");
+    $("#up").removeClass("current-link");
+    $("#custom").removeClass("current-link");
+    $("#resident-item").addClass("active");
+    $("#noresident-item").removeClass("active");
+    $("#up-item").removeClass("active");
+    $("#custom-item").removeClass("active");
 });
-// $("#noresident").click(function (e) {
-//     e.preventDefault();
-//     $("#noresident").toggleClass("current-link");
-//     $("#resident").toggleClass("current-link");
-//     $("#noresident-item").toggleClass("active");
-//     $("#resident-item").toggleClass("active");
-// });
+$("#noresident").click(function (e) {
+    e.preventDefault();
+    $("#noresident").addClass("current-link");
+    $("#resident").removeClass("current-link");
+    $("#up").removeClass("current-link");
+    $("#custom").removeClass("current-link");
+    $("#noresident-item").addClass("active");
+    $("#resident-item").removeClass("active");
+    $("#up-item").removeClass("active");
+    $("#custom-item").removeClass("active");
+});
+$("#up").click(function (e) {
+    e.preventDefault();
+    $("#up").addClass("current-link");
+    $("#resident").removeClass("current-link");
+    $("#noresident").removeClass("current-link");
+    $("#custom").removeClass("current-link");
+    $("#up-item").addClass("active");
+    $("#resident-item").removeClass("active");
+    $("#noresident-item").removeClass("active");
+    $("#custom-item").removeClass("active");
+});
+$("#custom").click(function (e) {
+    e.preventDefault();
+    $("#custom").addClass("current-link");
+    $("#resident").removeClass("current-link");
+    $("#noresident").removeClass("current-link");
+    $("#up").removeClass("current-link");
+    $("#custom-item").addClass("active");
+    $("#resident-item").removeClass("active");
+    $("#noresident-item").removeClass("active");
+    $("#up-item").removeClass("active");
+});
 
 $('body').on('click','#showmore.more',function(e){
     e.preventDefault();
@@ -64,6 +93,25 @@ $('body').on('click','#showmore.more',function(e){
 $('body').on('click','#showmore.less',function(e){
     e.preventDefault();
     $('.operations__documentation-kind.active>.operations__documentation-file').filter('[data-show="no"]').each(function () {
+        $(this).addClass('hide');
+        });
+    $('#showmore.less').removeClass('less').addClass('more').html('Смотреть все<svg class="more-picture"><use href="./src/svg/sprite/symbol-defs.svg#icon-more" /></svg>');
+});
+
+// create.html for
+
+$('body').on('click','#showmore.more',function(e){
+    e.preventDefault();
+    $('.documentation__list>.documentation__item.hide').each(function () {
+        $(this).removeClass('hide');
+    });
+    $('#showmore.more').addClass('less').removeClass('more').html('Скрыть<svg class="more-picture"><use href="./src/svg/sprite/symbol-defs.svg#icon-less" /></svg>');
+
+});
+
+$('body').on('click','#showmore.less',function(e){
+    e.preventDefault();
+    $('.documentation__list>.documentation__item').filter('[data-show="no"]').each(function () {
         $(this).addClass('hide');
         });
     $('#showmore.less').removeClass('less').addClass('more').html('Смотреть все<svg class="more-picture"><use href="./src/svg/sprite/symbol-defs.svg#icon-more" /></svg>');
